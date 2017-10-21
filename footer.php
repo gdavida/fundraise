89f14254650ea5047460feb994b070e91b503f2f
Davida Gaffney <gdavida@gmail.com>
2017-10-21 15:03:03 -0500

    don't need updated footer bc it will stay a template page

--- footer.php
+++ footer.php
@@ -11,33 +11,21 @@
 
 ?>
 
-	</div><!-- #content -->
+  </div><!-- #content -->
 
-<footer class="page-footer" id="colophon" class="site-footer">
-      <div class="container site-info">
-        <div class="row">
-          <div class="col l8 s12">
-            <h5 class="white-text">The problem</h5>
-            <p class="grey-text text-lighten-4">Christians in Myanmar face religious persection, extreme poverty, and limited educational opportunities.</p>
-            <a class="btn waves-effect waves-light red lighten-3" target="_blank" href="https://gitter.im/Dogfalo/materialize">Chat</a>
-          </div>
-          <div class="col l4 s12" style="overflow: hidden;">
-            <h5 class="white-text">Our Goal</h5>
-            <h5>$10,000</h5>
-            <div id="progress-tracker" style="height: 10px; border: 1px solid black; margin-bottom: 0">
-              <div style="width:50%; background-color: gray; height: 100%;"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="footer-copyright">
-        <div class="container">
-        © 2014-2017 Materialize, All rights reserved.
-        <a class="grey-text text-lighten-4 right" href="https://github.com/Dogfalo/materialize/blob/master/LICENSE">MIT License</a>
-        </div>
-      </div>
-    </footer>
-
+  <footer id="colophon" class="site-footer">
+    <div class="site-info">
+      <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fundraise17' ) ); ?>"><?php
+        /* translators: %s: CMS name, i.e. WordPress. */
+        printf( esc_html__( 'Proudly powered by %s', 'fundraise17' ), 'WordPress' );
+      ?></a>
+      <span class="sep"> | </span>
+      <?php
+        /* translators: 1: Theme name, 2: Theme author. */
+        printf( esc_html__( 'Theme: %1$s by %2$s.', 'fundraise17' ), 'fundraise17', '<a href="http://underscores.me/">DC Gaffney</a>' );
+      ?>
+    </div><!-- .site-info -->
+  </footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
