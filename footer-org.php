e498d888a8361e8cf0cfbef977757c37e00018dd
Davida Gaffney <gdavida@gmail.com>
2017-10-22 13:43:38 -0500

    added form to page and removed double pulling in of jquery

--- footer-org.php
+++ footer-org.php
@@ -42,7 +42,7 @@
 
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
+<? php wp_footer(); ?>
 
 </body>
 </html>
--- page.php
+++ page.php
@@ -15,7 +15,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="main" class="site-main container">
 
 			<?php
 			while ( have_posts() ) : the_post();
@@ -34,5 +34,4 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
