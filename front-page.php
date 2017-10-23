8e85a9b36728e18ec539beb78d92a1f20506fd1c
Davida Gaffney <gdavida@gmail.com>
2017-10-23 12:20:23 -0500

    fixed name of front-page

---
+++ front-page.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package fundraise17
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<p>You work hard to put together fundraisers, plan events, coordinate volunteers, and build a community of support for your organization.</p>
+
+			<p>When you talk about your organization, your passion shows.</p>
+
+			<p>People admire your enthusiasm, love the vision, and want to help. But how do people get involved?</p>
+
+			<p>How can people use their talents and passions to contribute?</p>
+
+			<p>How do you tap into your community's ?</p>
+
+			<p>How do you leverage passion, talent, time, and funds to increase community involvement and build your organization?</p>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
