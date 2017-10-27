6a268f266db2c93277dc45f737ab18f2cdc79546
Davida Gaffney <gdavida@gmail.com>
2017-10-27 12:11:34 -0500

    init commit

--- functions.php
+++ functions.php
@@ -161,6 +161,12 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 
+
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
 // Register Custom Post Type
 function organization() {
 
