af93fa2b114836343b3d72e9ff5c15a47634e609
Davida Gaffney <gdavida@gmail.com>
2017-10-27 12:18:12 -0500

    replace woocommerce support by using hook instead of page

--- functions.php
+++ functions.php
@@ -161,12 +161,30 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 }
 
 
-
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-
+// hook to add in woocommerce
 add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
 add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<main id="main">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</main>';
+}
+
+//hide does not support message
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+
+
+
+
+
+
 // Register Custom Post Type
 function organization() {
 
